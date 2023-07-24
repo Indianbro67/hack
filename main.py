2aed884d6b17e9e04fcfc4bbf64b511919bde199
Indianbro67 <133229712+Indianbro67@users.noreply.github.com>
2023-07-24 11:35:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,18 +17,32 @@ client = TelegramClient(
     api_hash,
 )
 
-client.start()
+async def get_group_entity():
+    # Replace YOUR_GROUP_USERNAME with the username of the private group you want to download from
+    group_username = 'YOUR_GROUP_USERNAME'
+    group_entity = await client.get_entity(group_username)
+    return group_entity
 
-@client.on(events.NewMessage)
-async def downloader(event):
-    # Check if the message is from a private chat and contains a photo
-    if event.is_private and event.photo and event.out is False:
-        result = await event.download_media()
-        # You can add custom logic here to handle the downloaded image
-        print(f"Downloaded image: {result}")
-        
-        # Upload the downloaded image to your saved messages
-        await client.send_file('me', result, caption="Uploaded image from a private chat!")
+async def main():
+    await client.start()
+    logger.info("Client is ready.")
+    
+    # Get the group entity
+    group_entity = await get_group_entity()
 
-asyncio.get_event_loop().run_forever()
-client.run_until_disconnected()
+    @client.on(events.NewMessage(chats=group_entity))
+    async def downloader(event):
+        # Check if the message contains media
+        if event.media:
+            result = await event.download_media()
+            # You can add custom logic here to handle the downloaded media
+            print(f"Downloaded media from the group: {result}")
+
+            # Upload the downloaded media to your saved messages
+            await client.send_file('me', result, caption="Uploaded media from the group!")
+
+    logger.info("Bot is running...")
+    await client.run_until_disconnected()
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
