2fcf6c3cd461a7fcdf890edf41aea8e56104b408
Indianbro67 <133229712+Indianbro67@users.noreply.github.com>
2023-07-26 15:32:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,20 +19,10 @@ client = TelegramClient(
 
 client.start()
 
-# Replace the following with the desired private group chat ID
-desired_group_chat_id = 1914730364
-
-@client.on(events.NewMessage(chats=desired_group_chat_id))
+@client.on(events.NewMessage)
 async def downloader(event):
     result = await event.download_media()
     # You can add custom logic here to handle the downloaded media
-    print(f"Downloaded media from chat {desired_group_chat_id}: {result}")
-    
-    # Upload the downloaded image to your saved messages
-    await client.send_file('me', result, caption=f"Uploaded media from chat {desired_group_chat_id}!")
 
-async def main():
-    await client.run_until_disconnected()
-
-if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
+asyncio.get_event_loop().run_forever()
+client.run_until_disconnected()
