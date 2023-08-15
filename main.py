e4e7684d7426ff9d9a114d9e92a3721e4127017c
Indianbro67 <133229712+Indianbro67@users.noreply.github.com>
2023-08-15 15:41:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# -*- coding:utf-8 -*-
 import asyncio
 import logging
+import os
 from decouple import config
 from telethon import TelegramClient, events
 
@@ -17,25 +17,22 @@ client = TelegramClient(
     api_hash,
 )
 
-async def upload_file_to_saved_messages(file_path):
-    try:
-        await client.send_file('me', file=file_path, caption='File saved in my messages.')
-        logger.info(f"File {file_path} uploaded to Saved Messages.")
-    except Exception as e:
-        logger.error(f"Failed to upload file: {e}")
+client.start()
 
 @client.on(events.NewMessage)
 async def downloader(event):
-    result = await event.download_media()
-    # You can add custom logic here to handle the downloaded media
+    # Check if the message is from a private chat and contains media (photo or video)
+    if event.is_private and event.media and event.out is False:
+        result = await event.download_media()
+        
+        try:
+            # Upload the downloaded media (photo or video) to saved messages
+            await client.send_file('me', result)
+            
+            print(f"Downloaded media: {result} and uploaded to saved messages")
+        finally:
+            # Delete the downloaded file, whether uploading succeeds or not
+            os.remove(result)
 
-    # Upload the downloaded file to Saved Messages
-    await upload_file_to_saved_messages(result)
-
-async def main():
-    await client.start()
-    logger.info("Client started.")
-    await client.run_until_disconnected()
-
-if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
+asyncio.get_event_loop().run_forever()
+client.run_until_disconnected()
